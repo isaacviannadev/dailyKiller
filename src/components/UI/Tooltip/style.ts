fd1d4deefff591b180ff5f53f7b08d00fcf86cd9
Isaac Vianna <isaacsvianna@gmail.com>
2022-09-21 11:02:07 -0300

    fix tooltip contrast

--- src/components/UI/Tooltip/style.ts
+++ src/components/UI/Tooltip/style.ts
@@ -5,8 +5,8 @@ export const TooltipContentSC = styled(Tooltip.Content)`
   ${({ theme }) => css`
     display: inline-block;
     padding: 0.5rem 1rem;
-    background-color: ${theme.color.main[400]};
-    color: ${theme.color.secondary[700]};
+    background-color: ${theme.color.main[500]};
+    color: ${theme.color.background.light};
     font-weight: bold;
     border-radius: 4px;
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
